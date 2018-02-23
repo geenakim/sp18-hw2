65366f6bb614f218108f97df2dd20b69d9ef46f4
geenakim <geenakim@berkeley.edu>
2018-02-22 18:00:24 -0800

    finished hw2

--- app/controllers/concerns/person.rb
+++ app/controllers/concerns/person.rb
@@ -4,21 +4,24 @@ class Person
   def initialize(name, age)
     @name = name
     @age = age
+    @nickname = name[0,4]
   end
 
   def nickname
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @nickname
   end
 
   def birth_year
     # YOUR IMPLEMENTATION HERE
-    1990
+    current_year = Time.now.year
+    birth_year = current_year - @age.to_i
+    birth_year
   end
 
   def introduction
     # YOUR IMPLEMENTATION HERE
-    'delete_me'
+    @name + " is " + @age + " years old"
   end
 
 end
